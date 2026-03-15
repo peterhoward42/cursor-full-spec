734bf3a93db726d00fec900473d25cf047eca844
peterhoward42 <peterhoward42@gmail.com>
2026-03-15 22:19:31 +0000

    Write spec for gitignore

--- cmd/function/main.go
+++ cmd/function/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
+	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/pete/cursor-full-spec/internal/app"
 )
 
 func main() {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("storage.NewClient: %v", err)
+	}
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Printf("storage client Close: %v", err)
+		}
+	}()
+
+	bucket := os.Getenv("GCS_BUCKET")
+	if bucket == "" {
+		log.Fatal("GCS_BUCKET environment variable is not set")
+	}
+
 	deps := app.Dependencies{
-		EventStorer: &app.FakeEventStorer{},
-		EventGetter: &app.FakeEventGetter{},
+		EventStorer: &app.GCSEventStorer{Bucket: bucket, Client: client},
+		EventGetter: &app.GCSEventGetter{Bucket: bucket, Client: client},
 	}
 	a := app.NewApplication(deps)
 	functions.HTTP("Function", a.ServeHTTP)
---
+++ docs/prompts/150-gitignore.md
@@ -0,0 +1,9 @@
+# Create a .gitignore file
+
+## Instructions
+
+- Create a .gitignore file for this repo
+- Include typical contents for a Go project
+- Include typical contents for a macos context
+- Include typical contents for a VSCode context
+- Include typical contents for a Cursor context
